cac51b9fa667db44bada43877dcad7ea3b2e218f
jonathanguyen21 <119833065+jonathanguyen21@users.noreply.github.com>
2023-07-27 13:29:19 -0700

    Created hero component

---
+++ components/Hero.jsx
@@ -0,0 +1,12 @@
+function Hero() {
+  return (
+    <section className="hero">
+      <a href="#">
+        <h1>Jonathan Nguyen</h1>
+      </a>
+      <h2>Software Engineer</h2>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "normalize.css";
+import "./App.css";
+
+function App() {
+  console.log("hello");
+  return (
+    <section className="hero">
+      <a href="#">
+        <h1>Jonathan Nguyen</h1>
+      </a>
+      <h2>Software Engineer</h2>
+    </section>
+  );
+}
+
+export default App;
