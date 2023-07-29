22f7448daac360448520d6f623acb59dbf460b90
jonathanguyen21 <119833065+jonathanguyen21@users.noreply.github.com>
2023-07-28 18:15:19 -0700

    Created skills section component

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,9 +1,13 @@
 import {
   faCss3Alt,
   faHtml5,
+  faJava,
   faJs,
-  faNodeJs,
+  faPython,
+  faSquareGit,
+  faSwift,
 } from "@fortawesome/free-brands-svg-icons";
+import { faC } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Skills() {
@@ -18,9 +22,30 @@ function Skills() {
         <FontAwesomeIcon icon={faHtml5} className="icon" />
         <p className="icon__name">HTML5</p>
       </div>
+
       <div className="skills__icon">
         <FontAwesomeIcon icon={faJs} className="icon" />
         <p className="icon__name">JavaScript</p>
+      </div>
+
+      <div className="skills__icon">
+        <FontAwesomeIcon icon={faPython} className="icon" />
+        <p className="icon__name">Python</p>
+      </div>
+
+      <div className="skills__icon">
+        <FontAwesomeIcon icon={faC} className="icon" />
+        <p className="icon__name">C++</p>
+      </div>
+
+      <div className="skills__icon">
+        <FontAwesomeIcon icon={faSquareGit} className="icon" />
+        <p className="icon__name">Git</p>
+      </div>
+
+      <div className="skills__icon">
+        <FontAwesomeIcon icon={faJava} className="icon" />
+        <p className="icon__name">Java</p>
       </div>
     </section>
   );
