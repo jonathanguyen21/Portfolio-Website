268b9f35c02f8862f445bbafb17a804ce4576bf5
jonathanguyen21 <119833065+jonathanguyen21@users.noreply.github.com>
2023-08-04 16:05:31 -0700

    fixed some mobile functions and added email

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -8,7 +8,17 @@ export const Contact = () => {
           action="https://formsubmit.co/jonathandnguyen21@gmail.com"
           method="POST"
         >
-          <h3>GET IN TOUCH</h3>
+          <h3 className="contact__h3">GET IN TOUCH</h3>
+          <h4 className="contact__h4">
+            Reachable at:{" "}
+            <a
+              href="https://mail.google.com/mail/?view=cm&fs=1&to=jonathandnguyen21@gmail.com"
+              target="_blank"
+              className="contact__email"
+            >
+              jonathandnguyen21@gmail.com
+            </a>
+          </h4>
           <input type="text" name="name" placeholder="Your Name *" required />
           <input
             type="email"
