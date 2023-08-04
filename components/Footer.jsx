5c15615447755effc33344cb40774a539c78d7f8
jonathanguyen21 <119833065+jonathanguyen21@users.noreply.github.com>
2023-08-04 16:48:25 -0700

    added footer

---
+++ components/Footer.jsx
@@ -0,0 +1,12 @@
+export const Footer = () => {
+  return (
+    <section className="footer container">
+      <p>
+        Designed by <strong>Jonathan Nguyen</strong> using the{" "}
+        <strong>React</strong> library. Styled with{" "}
+        <strong>Framer Motion</strong> for animations and <strong>CSS</strong>{" "}
+        for all other styling. Deployed using <strong>Vercel</strong>.
+      </p>
+    </section>
+  );
+};
--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import About from "../components/About";
 import Projects from "../components/Projects";
 import SocialMedia from "../components/SocialMedia";
 import { Contact } from "../components/Contact";
+import { Footer } from "../components/Footer";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Projects />
       <Contact />
       <SocialMedia />
+      <Footer />
     </>
   );
 }
